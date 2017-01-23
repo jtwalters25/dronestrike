e3843b89c11973e923bb2fc1918a5d90573a4562
radenska <yana_radenska@yahoo.com>
2017-01-23 12:18:40 -0800

    created dataController.js and data.js files

---
+++ public/scripts/controllers/dataController.js
@@ -0,0 +1,59 @@
+'use strict';
+
+(function(module) {
+  var articleController = {};
+
+  Article.createTable();
+
+  articleController.index = function(ctx, next) {
+    if(ctx.articles.length) {
+      articleView.index(ctx.articles);
+    } else{
+      page('/');
+    }
+  };
+
+  articleController.loadById = function(ctx, next) {
+    var articleData = function(article) {
+      ctx.articles = article;
+      next();
+    };
+    Article.findWhere('id', ctx.params.id, articleData);
+  };
+
+  articleController.loadByAuthor = function(ctx, next) {
+    var authorData = function(articlesByAuthor) {
+      ctx.articles = articlesByAuthor;
+      next();
+    };
+
+    Article.findWhere(
+      'author', ctx.params.authorName.replace('+', ' '), authorData
+    );
+  };
+
+  articleController.loadByCategory = function(ctx, next) {
+    var categoryData = function(articlesInCategory) {
+      ctx.articles = articlesInCategory;
+      next();
+    };
+
+    Article.findWhere('category', ctx.params.categoryName, categoryData);
+  };
+
+  articleController.loadAll = function(ctx, next) {
+    var articleData = function(allArticles) {
+      ctx.articles = Article.allArticles;
+      next();
+    };
+
+    if (Article.allArticles.length) {
+      ctx.articles = Article.allArticles;
+      next();
+    } else {
+      Article.fetchAll(articleData);
+    }
+  };
+
+  module.articleController = articleController;
+})(window);
