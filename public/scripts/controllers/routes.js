bb053a9914db458afee62c2cea11decc0141c6e6
Jeremiah <jeremiaht.walters@gmail.com>
2017-01-24 17:32:46 -0800

    routes

--- public/scripts/controllers/routes.js
+++ public/scripts/controllers/routes.js
@@ -1,29 +1,13 @@
-'use strict'
-
 'use strict';
 
 page('/',
-  dataController.loadAll,
-  dataController.index);
+Data.fetchAll,
+dataController.loadAll,
+dataController.index);
 
-// page('/about', aboutController.index);
-// page('/admin', adminController.index);
-
-page('/data/:_id',
-  dataController.loadById,
-  dataController.index);
-
-// Redirect home if the default filter option is selected:
-page('/category', '/');
-page('/author', '/');
-
-page('/author/:authorName',
-  dataController.loadByAuthor,
-  dataController.index);
-
-page('/category/:categoryName',
-  dataController.loadByCategory,
-  dataController.index);
+page('/map', mapController.index);
+page('/stats', chartView.makeChart);
+page('/about')
 
 page('*', function(){
   $('body').text('Not found!');
