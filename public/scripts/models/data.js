d33ae0168ece468a0e5cc769ef76c31669f487a1
radenska <yana_radenska@yahoo.com>
2017-01-26 02:25:15 -0800

    server request stall hell

--- public/scripts/models/data.js
+++ public/scripts/models/data.js
@@ -6,13 +6,14 @@
       this[val] = opts[val];
     }, this);
   }
+  var giantObject = {};
 
   Data.allData = [];
 
-  Data.prototype.insertRecord = function() {
-    console.log('this', this);
-    $.post('/strikes/insert', {number: this.number, country: this.country, date: this.date, narrative: this.narrative, town: this.town, location: this.location, deaths: this.deaths, deaths_min: this.deaths_min, deaths_max: this.deaths_max, civilians: this.civilians, injuries: this.injuries, children: this.children, tweet_id: this.tweet_id, bureau_id: this.bureau_id, bij_summary_short: this.bij_summary_short, bij_link: this.bij_link, target: this.target, lat: this.lat, lon: this.lon, names: this.names})
-  };
+  giantObject.insertRecord = function(obj) {
+    console.log('obj', obj);
+    $.post('/strikes/insert', {allstrikes: obj})
+  }
 
     Data.loadAll = function(rows) {
       console.log('in loadall');
@@ -27,7 +28,6 @@
           if (rows.length) {
             Data.loadAll(rows);
             console.log(rows.length, 'rows length');
-            let pakistan = rows.filter(strike => {return strike.country === 'Pakistan'});
             ctx.data = Data.allData;
             next();
           } else {
@@ -37,11 +37,17 @@
               dataType: 'jsonp'
             })
              .then(rawData => {
-               rawData.strike.forEach(function(item) {
-                 var strike = new Data(item);
-                 strike.insertRecord();
+               var tempData = rawData.strike.map(val => {
+                 return {number: val.number, country: val.country, lon: val.lon, lat: val.lat}
                });
-               Data.fetchAll();
+               let somalia = tempData.filter(val => {return val.country === 'Somalia'});
+               let yemen = tempData.filter(val => {return val.country === 'Yemen'});
+               let pakistan = tempData.filter(val => {return val.country.indexOf('P') === 0});
+               console.log('s', somalia, 'y', yemen, 'p', pakistan);
+               giantObject.insertRecord(JSON.stringify(somalia));
+              //  giantObject.insertRecord(JSON.stringify(yemen));
+              //  giantObject.insertRecord(JSON.stringify(pakistan));
+              //  Data.fetchAll();
              });
           }
         });
--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 'use strict'
 
 const pg = require('pg');
+pg.defaults.poolSize = 0;
 const express = require('express');
 const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 3000;
 const app = express();
 const conString = process.env.DATABASE_URL || 'postgres://localhost:5432';
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
+
 app.use(express.static('./public'));
 
 app.get('/map', function(request, response){
@@ -43,7 +45,7 @@ app.get('/strikes/all', (request, response) => {
       `SELECT * FROM strikes;`,
       (err, result) => {
         if (err) console.error(err);
-        response.send(result.rows);
+        response.send(result);
         client.end();
       }
     );
@@ -51,38 +53,25 @@ app.get('/strikes/all', (request, response) => {
 });
 
 app.post('/strikes/insert', (request, response) => {
+  console.log('request body', request.body);
   var client = new pg.Client(conString);
+  console.log('in strikes insert');
   client.connect(err => {
+    console.log('in strikes insert client connect')
     if (err) console.error(err);
     client.query(
-      `INSERT INTO strikes (number, country, date, narrative, town, location, deaths, deaths_min, deaths_max, civilians, injuries, children, tweet_id, bureau_id, bij_summary_short, bij_link, target, lat, lon, names) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20);`,
-      [ request.body.number,
-        request.body.country,
-        request.body.date,
-        request.body.narrative,
-        request.body.town,
-        request.body.location,
-        request.body.deaths,
-        request.body.deaths_min,
-        request.body.deaths_max,
-        request.body.civilians,
-        request.body.injuries,
-        request.body.children,
-        request.body.tweet_id,
-        request.body.bureau_id,
-        request.body.bij_summary_short,
-        request.body.bij_link,
-        request.body.target,
-        request.body.lat,
-        request.body.lon,
-        request.body.names],
+      `INSERT INTO strikes (allstrikes) VALUES ($1);`,
+      [ request.body ],
       err => {
         if (err) {
           console.error(err);
           response.send(err);
         }
+        console.log('in strikes insert at end of query');
+        client.end();
       });
   })
+  console.log('in strikes insert before final closing brackety thingies');
 })
 
 app.get('*', function(request, response){
